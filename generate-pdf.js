7ef63721757691faa5e6dfe77efc651309e09336
Fred Lecours <flecours@billets.ca>
2026-03-04 11:28:12 -0500

    fix

--- generate-pdf.js
+++ generate-pdf.js
@@ -2,7 +2,9 @@ const puppeteer = require("puppeteer");
 
 (async () => {
 
-const browser = await puppeteer.launch();
+const browser = await puppeteer.launch({
+  args: ['--no-sandbox', '--disable-setuid-sandbox']
+});
 
 const page = await browser.newPage();
 
