c02e36413319831d31ce2a02eada3457c550ad6f
Fred Lecours <flecours@billets.ca>
2026-03-04 11:25:44 -0500

    Initial CV

---
+++ README.md
@@ -0,0 +1,17 @@
+# Frédéric Doucet-Lecours – CV
+
+Developer resume built with HTML and CSS.
+
+## Live CV
+
+<https://fredopolo51.github.io/cv>
+
+## PDF
+
+Download the latest PDF version in this repository.
+
+## Tech
+
+- HTML
+- CSS
+- Puppeteer
---
+++ generate-pdf.js
@@ -0,0 +1,21 @@
+const puppeteer = require("puppeteer");
+
+(async () => {
+
+const browser = await puppeteer.launch();
+
+const page = await browser.newPage();
+
+await page.goto("file://" + __dirname + "/index.html", {
+waitUntil: "networkidle0"
+});
+
+await page.pdf({
+path: "cv.pdf",
+format: "A4",
+printBackground: true
+});
+
+await browser.close();
+
+})();
